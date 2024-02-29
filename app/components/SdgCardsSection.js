151a62982ffb35be5cffd928098d881965d4057a
Francis Dominic Fajardo <fajardo@duck.com>
2024-02-29 23:32:54 +0800

    feat: add final design for SDG cards section.

--- app/components/SdgCardsSection.js
+++ app/components/SdgCardsSection.js
@@ -1,11 +1,50 @@
+import Image from "next/image";
+
 function MainSdgCard() {
     return (
-        <div class="container bg-sky-900 p-4 rounded-3xl">
-            <div class="text-4xl font-bebas">
-            17
+        <div className="container bg-sky-900 px-8 py-8 rounded-3xl flex flex-col">
+            <div className="flex justify-between mb-8">
+                <div className="text-6xl font-bebas">
+                17
+                </div>
+                <Image
+                    src="/sdg-17-logo.svg"
+                    alt="SDG 17 Logo"
+                    width={128}
+                    height={128}
+                    priority
+                />
             </div>
-            <div class="text-4xl font-bebas">
-            Partnerships for the Goals
+            <div className="text-8xl text-right font-bebas w-3/5 self-end leading-none">
+                Partnerships for the Goals
+            </div>
+        </div>
+    );
+}
+
+function GlassCard(aProps) {
+    const { count, label, bgImage } = aProps;
+    return (
+        <div className="grid">
+            <div className="border-2 border-white/30 rounded-3xl p-5 backdrop-blur font-zen-dots grid-area-1 hover:backdrop-blur-none flex flex-col justify-end transition-all drop-shadow-xl hover:drop-shadow-md backdrop-brightness-75">
+                <div className="text-7xl">{count}</div>
+                <div className="text-4xl uppercase">{label}</div>
+            </div>
+            <div className="rounded-3xl bg-sky-900 grid-area-1 bg-cover"
+                style={{ backgroundImage: `url(${bgImage})` }}>
+            </div>
+        </div>
+    );
+}
+
+function SecondarySdgCard() {
+    return (
+        <div className="container flex w-full gap-4">
+            <GlassCard count="230" label="events" bgImage="/events.jpg" />
+            <div className="container flex flex-col gap-4">
+                <GlassCard count="2183" label="actions" bgImage="/actions.jpg" />
+                <GlassCard count="74" label="publications" bgImage="/publications.jpg" />
+                <GlassCard count="19" label="targets" bgImage="/targets2.jpg" />
             </div>
         </div>
     );
@@ -13,8 +52,9 @@ function MainSdgCard() {
 
 export default function SdgCardsSection() {
     return (
-        <div>
+        <div className="container w-full my-4 flex flex-col gap-6">
             <MainSdgCard />
+            <SecondarySdgCard />
         </div>
     );
 }
--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import TickerSection from "./components/TickerSection";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex max-w-5xl flex-col items-center justify-between p-24 m-auto">
       <TickerSection />
       <SdgCardsSection />
     </main>
