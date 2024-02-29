9e2d1de4fdee775c34f354d992622ce2da3ff5c1
Francis Dominic Fajardo <fajardo@duck.com>
2024-02-29 19:17:47 +0800

    feat: test card for SDG section.

--- app/components/SdgCardsSection.js
+++ app/components/SdgCardsSection.js
@@ -1,5 +1,20 @@
+function MainSdgCard() {
+    return (
+        <div class="container bg-sky-900 p-4 rounded-3xl">
+            <div class="text-4xl font-bebas">
+            17
+            </div>
+            <div class="text-4xl font-bebas">
+            Partnerships for the Goals
+            </div>
+        </div>
+    );
+}
+
 export default function SdgCardsSection() {
     return (
-        <div>Test</div>
+        <div>
+            <MainSdgCard />
+        </div>
     );
 }
