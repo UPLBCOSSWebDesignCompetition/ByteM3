84c4a1478d7412510f9c128d73237a4413e64891
Tarikuzuma <edwinjrgumba@gmail.com>
2024-03-02 22:30:02 +0800

    chore: changed font to zen

--- app/components/SdgArticlesSection.js
+++ app/components/SdgArticlesSection.js
@@ -14,11 +14,11 @@ function MainTitle() {
             
             <div class="relative flex items-center">
                 <span class="flex-shrink text-gray-400 mr-5"> <h1 class="mb-2 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl">
-                    <span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">More from the UN </span></h1>
+                    <span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400 ">More from the UN </span></h1>
                 </span>
                 <div className="flex-grow" style={{ borderTop: '5px solid gray' }}></div>
             </div> 
-            <p class="text-lg font-normal text-gray-500 lg:text-xl dark:text-gray-400">Featured Stories across the United Nations and our worldwide family or agencies, funds, and Programmes.</p>
+            <p className="text-lg font-normal  text-gray-500 lg:text-xl dark:text-gray-400">Featured Stories across the United Nations and our worldwide family or agencies, funds, and Programmes.</p>
         </div>
     );
 }
@@ -65,9 +65,9 @@ function ArticleComponent() {
 
                 return (
                     <article key={index} class="my-10">
-                        <div class="container flex-row  gap-10 grid lg:grid-cols-2">
+                        <div class="container flex-row gap-10 grid lg:grid-cols-2">
                             <div class="image">
-                                <img
+                                <img className = "rounded-3xl"
                                     src={article.article_image}
                                     alt="Plant photo"
                                     width={750}
@@ -76,16 +76,16 @@ function ArticleComponent() {
                                 />
                             </div>
                             <div class="text">
-                                <div className="article-title my-3">
-                                    <h2 class="text-5xl font-extrabold dark:text-white">
+                                <div className="article-title  my-3">
+                                    <h2 class="text-5xl font-extrabold  dark:text-white">
                                         <a href="#">{article.article_title}</a>
                                     </h2>
                                 </div>
-                                <div className="article-date my-4">
+                                <div className="article-date  my-4">
                                     <span class="font-semibold text-sm text-gray-500 dark:text-gray-400">{article.article_date}</span>
                                 </div>
-                                <div className="article-content my-4">
-                                    <p class="text-lg text-justify font-normal text-gray-500 dark:text-gray-400">{seeMore(articleContent, 650)} <span className="underline text-blue-500 hover:text-blue-700">See more...</span></p>
+                                <div className="article-content  my-4">
+                                    <p class="text-lg text-justify font-normal text-gray-500 dark:text-gray-400">{seeMore(articleContent, 550)} <span className="underline text-blue-500 hover:text-blue-700">See more...</span></p>
                                 </div>
                             </div>
                         </div>
@@ -114,7 +114,7 @@ function ArticleBottom(){
 
 export default function SdgArticlesSection() {
     return (
-        <div className="article-main w-full">
+        <div className="article-main w-full font-zen-dots">
             <MainTitle />
             <ArticleComponent />
             <ArticleBottom />
