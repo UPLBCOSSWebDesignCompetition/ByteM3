b77598a5110bc07c389fe333b9b72b7f1d74d433
Francis Dominic Fajardo <fajardo@duck.com>
2024-03-02 23:22:15 +0800

    ui: move footer out of main, adjust padding.

--- app/page.js
+++ app/page.js
@@ -5,11 +5,13 @@ import Footer from "./components/Footer";
 
 export default function Home() {
   return (
-    <main className="flex max-w-5xl flex-col items-center justify-between p-24 m-auto">
-      <TickerSection />
-      <SdgCardsSection />
-      <SdgArticlesSection />
+    <>
+      <main className="flex max-w-5xl flex-col items-center justify-between lg:px-24 px-5 m-auto">
+        <TickerSection />
+        <SdgCardsSection />
+        <SdgArticlesSection />
+      </main>
       <Footer />
-    </main>
+    </>
   );
 }
