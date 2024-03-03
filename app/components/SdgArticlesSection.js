c9b9eadb14b59b555072e4e0a0fc548717dda008
Tarikuzuma <edwinjrgumba@gmail.com>
2024-03-03 16:34:12 +0800

    ui: animation on every scroll

--- app/components/SdgArticlesSection.js
+++ app/components/SdgArticlesSection.js
@@ -1,4 +1,8 @@
+"use client"
+
 import Image from 'next/image'
+import articleData  from '../articles'
+import { AnimationOnScroll } from 'react-animation-on-scroll'
 
 // function to say "see more"  when character length is more than 100
 function seeMore(text, length) {
@@ -54,42 +58,39 @@ function ArticleComponent1(){
 }
 
 function ArticleComponent() {
-    const fs = require('fs');
-    const jsonData = fs.readFileSync('app/articles.json', 'utf8');
-    const articles = JSON.parse(jsonData);
-
     return (
         <div>
-            {articles.map((article, index) => {
+            {articleData.map((article, index) => {
                 const articleContent = article.article_description;
-
                 return (
-                    <article key={index} class="my-10">
-                        <div class="container flex-row gap-10 grid lg:grid-cols-2">
-                            <div class="image">
-                                <img className = "rounded-3xl"
-                                    src={article.article_image}
-                                    alt="Plant photo"
-                                    width={750}
-                                    height={500}
-                                    priority
-                                />
+                    <AnimationOnScroll animateIn="animate__fadeInLeft">
+                        <article key={index} class="my-10">
+                            <div class="container flex-row gap-10 grid lg:grid-cols-2">
+                                <div class="image cursor-pointer">
+                                    <img className = "rounded-3xl"
+                                        src={article.article_image}
+                                        alt="Plant photo"
+                                        width={750}
+                                        height={500}
+                                        priority
+                                    />
+                                </div>
+                                <div class="text">
+                                    <div className="article-title  my-3">
+                                        <h2 class="text-4xl font-extrabold  dark:text-white">
+                                            <a href="#">{article.article_title}</a>
+                                        </h2>
+                                    </div>
+                                    <div className="article-date  my-4">
+                                        <span class="font-semibold text-sm text-gray-500 dark:text-gray-400">{article.article_date}</span>
+                                    </div>
+                                    <div className="article-content  my-4">
+                                        <p class="text-lg text-justify font-normal text-gray-500 dark:text-gray-400">{seeMore(articleContent, 30)}… <span className="underline text-gray-300 cursor-pointer">See more...</span></p>
+                                    </div>
+                                </div>
                             </div>
-                            <div class="text">
-                                <div className="article-title  my-3">
-                                    <h2 class="text-5xl font-extrabold  dark:text-white">
-                                        <a href="#">{article.article_title}</a>
-                                    </h2>
-                                </div>
-                                <div className="article-date  my-4">
-                                    <span class="font-semibold text-sm text-gray-500 dark:text-gray-400">{article.article_date}</span>
-                                </div>
-                                <div className="article-content  my-4">
-                                    <p class="text-lg text-justify font-normal text-gray-500 dark:text-gray-400">{seeMore(articleContent, 550)} <span className="underline text-blue-500 hover:text-blue-700">See more...</span></p>
-                                </div>
-                            </div>
-                        </div>
-                    </article>
+                        </article>
+                    </AnimationOnScroll>
                 );
             })}
         </div>
@@ -100,7 +101,7 @@ function ArticleBottom(){
     return (
         <div className="article-top my-20 font ">
             
-            <div class="relative flex items-center">
+            <div class="relative flex items-center cursor-pointer">
                 <div className="flex-grow" style={{ borderTop: '5px solid gray' }}></div>
                     <span class="flex-shrink text-gray-400 ml-5 mr-5"> <h3 class="mb-1 text-xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-4xl">
                         <span class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Load More</span></h3>
