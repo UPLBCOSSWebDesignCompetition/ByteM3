337c5eae2a567e5fb4c2ef4bb79a3a01f1d45d20
Francis Dominic Fajardo <fajardo@duck.com>
2024-03-03 17:36:29 +0800

    ui: fix responsive layout of SDG cards.

--- app/components/SdgCardsSection.js
+++ app/components/SdgCardsSection.js
@@ -17,7 +17,7 @@ function MainSdgCard() {
                     height={128}
                 />
             </div>
-            <div className="text-8xl text-right font-bebas w-3/5 self-end leading-none">
+            <div className="text-5xl md:text-8xl text-right font-bebas md:w-3/5 self-end leading-none">
                 Partnerships for the Goals
             </div>
         </div>
@@ -30,7 +30,7 @@ function GlassCard(aProps) {
         <div className="grid">
             <div className="border-2 border-white/30 rounded-3xl p-5 backdrop-blur font-zen-dots grid-area-1 hover:backdrop-blur-none flex flex-col justify-end transition-all drop-shadow-xl hover:drop-shadow-md backdrop-brightness-75 cursor-pointer">
                 <div className="text-7xl">{count}</div>
-                <div className="text-4xl uppercase">{label}</div>
+                <div className="text-2xl md:text-4xl uppercase">{label}</div>
             </div>
             <div className="rounded-3xl bg-sky-900 grid-area-1 bg-cover"
                 style={{ backgroundImage: `url(${bgImage})` }}>
@@ -41,7 +41,7 @@ function GlassCard(aProps) {
 
 function SecondarySdgCard() {
     return (
-        <div className="container flex w-full gap-4">
+        <div className="container flex flex-col md:flex-row w-full gap-4">
             <GlassCard count="230" label="events" bgImage="/events.jpg" />
             <div className="container flex flex-col gap-4">
                 <GlassCard count="2183" label="actions" bgImage="/actions.jpg" />
