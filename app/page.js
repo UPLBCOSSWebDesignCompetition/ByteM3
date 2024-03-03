38ab4d6781a41cb3cb2ae25a52b23a06f56b5ba3
jehosubedi <jehosubedi4@gmail.com>
2024-03-03 18:55:13 +0800

    added cursor effect

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 import Header from "./components/Header";
+import Cursor from "./components/Cursor";
 import SdgCardsSection from "./components/SdgCardsSection";
 import TickerSection from "./components/TickerSection";
 import SdgArticlesSection from "./components/SdgArticlesSection";
@@ -9,6 +10,7 @@ import HeroSection from "./components/HeroSection";
 export default function Home() {
   return (
     <>
+      <Cursor />
       <Header />
       <main className="flex max-w-5xl flex-col items-center justify-between lg:px-24 px-5 m-auto">
         <HeroSection />
