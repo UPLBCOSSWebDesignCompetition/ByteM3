830b822874e738456fdf7a952451a77987bc7796
jehosubedi <jehosubedi4@gmail.com>
2024-03-03 17:51:20 +0800

    fixed header

--- app/components/Header.js
+++ app/components/Header.js
@@ -4,7 +4,7 @@ const Header = () => {
     return (
         <header className='top-0 z-[20] mx-auto flex w-full justify-between'> 
             <img src="/logo3.svg" alt="Logo" width={75} height={75} />
-            <div className="flex-grow justify-center" style={{ borderTop: '5px solid white' }}></div>
+            <div className="flex-grow self-center mr-5" style={{ borderTop: '5px solid white' }}></div>
             <img src="/menu.svg" alt="Menu" width={37} height={37}/>
             <div className='z-50 fixed top-[90px] right-[335px] cursor-pointer '>
             </div>
