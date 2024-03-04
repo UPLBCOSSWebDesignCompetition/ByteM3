275f07ce42b9092a35596138f1381028d98454df
Francis Dominic Fajardo <fajardo@duck.com>
2024-03-04 21:59:45 +0800

    ui: implement arrow on footer menu link hover.

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,39 +1,51 @@
 // components/Footer.js
 
+import Image from "next/image";
 import Link from "next/link";
 
+function FooterMenuItem(aProps) {
+    const { label, href } = aProps;
+    return (
+        <li>
+            <Link href={href} className="flex gap-5 group">
+                <Image
+                    className="absolute opacity-0 group-hover:opacity-100 transition-all"
+                    src="/arrow-hex.svg"
+                    width="32"
+                    height="32"
+                />
+                <span className="hover:text-gray-300 cursor-pointer text-2xl md:text-4xl uppercase group-hover:ml-12 transition-all">
+                    {label}
+                </span>
+            </Link>
+        </li>
+    );
+}
+
 function Footer() {
+    const menuItems = [
+        "Home",
+        "About",
+        "Resources",
+        "Contact Us",
+    ];
+    
     return (
         <footer className="bg-grey text-white py-8 w-full relative">
-            <div className="container mx-auto bg-grey py-8">
-                <div className="grid">
-                    <div>
-                        <ul className="mt-2 px-10 font-zen-dots"> 
-                            <li className="mb-2">
-                                <Link href="/">
-                                    <span className="hover:text-gray-300 cursor-pointer text-4xl uppercase">Home</span>
-                                </Link>
-                            </li>
-                            <li className="mb-2">
-                                <Link href="/">
-                                    <span className="hover:text-gray-300 cursor-pointer text-4xl uppercase">About</span>
-                                </Link>
-                            </li>
-                            <li className="mb-2">
-                                <Link href="/">
-                                    <span className="hover:text-gray-300 cursor-pointer text-4xl uppercase">Resources</span>
-                                </Link>
-                            </li>
-                            <li>
-                                <Link href="/">
-                                    <span className="hover:text-gray-300 cursor-pointer text-4xl uppercase">Contact Us</span>
-                                </Link>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
+            <div className="container mx-auto bg-grey mb-8">
+                <ul className="mt-2 px-10 font-zen-dots flex flex-col gap-2"> 
+                    {
+                        menuItems.map(function(aItem, aIndex) {
+                            return (
+                                <FooterMenuItem label={aItem} href="#" key={aIndex} />
+                            );
+                        })
+                    }
+                </ul>
             </div>
-            <p className="text-center text-xl font-bebas uppercase">&copy; 2024 BYTEM3! ALL RIGHTS RESERVED</p>
+            <p className="text-center text-xl font-bebas uppercase">
+                &copy; 2024 BYTEM3! ALL RIGHTS RESERVED
+            </p>
         </footer>
     );
 };
